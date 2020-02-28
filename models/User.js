ee78c8f8268db5e92226e2d036853b797d61e29f
Leah Migacz <lmigacz@msudenver.edu>
2020-02-28 14:13:13 -0700

    added components

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+const db = require('../database/db.js');
+
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    first_name: {
+      type: DataTypes.STRING
+    },
+    last_name: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    created: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  },
+  {
+    timestamps: false
+  })
+  return User;
+}
