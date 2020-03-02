f9f011098d35f8c546bafbc35faf409edda49b3b
mjahere023@gmail.com <mjahere023@gmail.com>
2020-03-02 14:53:09 -0700

    altered db file.

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = {};
-const sequelize = new Sequelize('sunscraper', 'root', 'greenp0ison', {
+const sequelize = new Sequelize('sunscraper', 'root', 'icy263v##out', {
   host: 'localhost',
   dialect: 'mysql',
   operatorsAliases: false,
