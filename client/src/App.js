8ab8a5d2fa0c31e2a8d88b9cb22dd200a892bf3f
mjahere023@gmail.com <mjahere023@gmail.com>
2020-03-03 09:31:05 -0700

    Added dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Profile from './components/Profile';
 import Input from './components/Input';
+import Dashboard from './components/Dashboard'
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
             <Route exact path="/login" component={Login} />
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/input" component={Input} />
+            <Route exact path="/Dashboard" component= {Dashboard}/>
           </div>
         </div>
       </Router>
